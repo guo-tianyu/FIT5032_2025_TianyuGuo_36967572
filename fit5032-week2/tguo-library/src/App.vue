347220ffa709c6d0a74ac64aebdb4d777662222a
GUO0926 <3296692668@qq.com>
2026-07-12 22:35:08 +0800

    Complete Bootstrap responsive form for eFolio Task 3

--- fit5032-week2/tguo-library/src/App.vue
+++ fit5032-week2/tguo-library/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
-import JSON from './components/JSON.vue'
+//import JSON from './components/JSON.vue'
+import Form from './components/Form.vue'
 </script>
 
 <template>
-  <JSON />
+  <!-- <JSON /> -->
+  <Form />
 </template>
