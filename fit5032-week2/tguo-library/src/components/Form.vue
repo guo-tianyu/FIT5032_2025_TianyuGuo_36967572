4586527857b8c5e9e3b9c91a49175820ffc1781e
GUO0926 <3296692668@qq.com>
2026-07-17 21:38:41 +0800

    Complete Lab 4 validation and PrimeVue DataTable

--- fit5032-week2/tguo-library/src/components/Form.vue
+++ fit5032-week2/tguo-library/src/components/Form.vue
@@ -14,8 +14,13 @@
                 type="text"
                 class="form-control"
                 id="username"
+                @blur="() => validateName(true)"
+                @input="() => validateName(false)"
                 v-model="formData.username"
               >
+              <div v-if="errors.username" class="text-danger">
+                {{ errors.username }}
+              </div>
             </div>
 
             <div class="col-sm-6">
@@ -26,8 +31,13 @@
                 type="password"
                 class="form-control"
                 id="password"
+                @blur="() => validatePassword(true)"
+                @input="() => validatePassword(false)"
                 v-model="formData.password"
               >
+              <div v-if="errors.password" class="text-danger">
+                {{ errors.password }}
+              </div>
             </div>
           </div>
 
@@ -38,11 +48,15 @@
                   type="checkbox"
                   class="form-check-input"
                   id="isAustralian"
+                  @change="() => validateResident(true)"
                   v-model="formData.isAustralian"
                 >
                 <label for="isAustralian" class="form-check-label">
                   Australian Resident?
                 </label>
+                <div v-if="errors.resident" class="text-danger">
+                  {{ errors.resident }}
+                </div>
               </div>
             </div>
 
@@ -53,12 +67,17 @@
               <select
                 class="form-select"
                 id="gender"
+                @blur="() => validateGender(true)"
+                @change="() => validateGender(false)"
                 v-model="formData.gender"
               >
                 <option value="male">Male</option>
                 <option value="female">Female</option>
                 <option value="other">Other</option>
               </select>
+              <div v-if="errors.gender" class="text-danger">
+                {{ errors.gender }}
+              </div>
             </div>
           </div>
 
@@ -70,8 +89,13 @@
               class="form-control"
               id="reason"
               rows="3"
+              @blur="() => validateReason(true)"
+              @input="() => validateReason(false)"
               v-model="formData.reason"
             ></textarea>
+            <div v-if="errors.reason" class="text-danger">
+              {{ errors.reason }}
+            </div>
           </div>
 
           <div class="text-center">
@@ -88,39 +112,19 @@
             </button>
           </div>
         </form>
-        <!-- Activity 6: submitted information cards -->
-        <div class="row mt-5" v-if="submittedCards.length">
-          <div class="d-flex flex-wrap justify-content-start">
-            <div
-              v-for="(card, index) in submittedCards"
-              :key="index"
-              class="card m-2"
-              style="width: 18rem;"
-            >
-              <div class="card-header">
-                User Information
-              </div>
-
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">
-                  Username: {{ card.username }}
-                </li>
-                <li class="list-group-item">
-                  Password: {{ card.password }}
-                </li>
-                <li class="list-group-item">
-                  Australian Resident:
-                  {{ card.isAustralian ? 'Yes' : 'No' }}
-                </li>
-                <li class="list-group-item">
-                  Gender: {{ card.gender }}
-                </li>
-                <li class="list-group-item">
-                  Reason: {{ card.reason }}
-                </li>
-              </ul>
-            </div>
-          </div>
+        <div class="mt-5" v-if="submittedCards.length">
+          <h2 class="text-center mb-3">Submitted User Information</h2>
+          <DataTable :value="submittedCards" tableStyle="min-width: 50rem">
+            <Column field="username" header="Username"></Column>
+            <Column field="password" header="Password"></Column>
+            <Column header="Australian Resident">
+              <template #body="{ data }">
+                {{ data.isAustralian ? 'Yes' : 'No' }}
+              </template>
+            </Column>
+            <Column field="gender" header="Gender"></Column>
+            <Column field="reason" header="Reason for joining"></Column>
+          </DataTable>
         </div>
       </div>
     </div>
@@ -130,6 +134,8 @@
 <script setup>
 // Activity 6 会在这里添加 formData、submitForm 和 clearForm
 import { ref } from 'vue'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
 
 const formData = ref({
   username: '',
@@ -141,10 +147,87 @@ const formData = ref({
 
 const submittedCards = ref([])
 
+const errors = ref({
+  username: null,
+  password: null,
+  resident: null,
+  gender: null,
+  reason: null
+})
+
+const validateName = (blur) => {
+  if (formData.value.username.length < 3) {
+    if (blur) errors.value.username = 'Name must be at least 3 characters'
+  } else {
+    errors.value.username = null
+  }
+}
+
+const validatePassword = (blur) => {
+  const password = formData.value.password
+  const minLength = 8
+  const hasUppercase = /[A-Z]/.test(password)
+  const hasLowercase = /[a-z]/.test(password)
+  const hasNumber = /\d/.test(password)
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password)
+
+  if (password.length < minLength) {
+    if (blur) errors.value.password = `Password must be at least ${minLength} characters long.`
+  } else if (!hasUppercase) {
+    if (blur) errors.value.password = 'Password must contain at least one uppercase letter.'
+  } else if (!hasLowercase) {
+    if (blur) errors.value.password = 'Password must contain at least one lowercase letter.'
+  } else if (!hasNumber) {
+    if (blur) errors.value.password = 'Password must contain at least one number.'
+  } else if (!hasSpecialChar) {
+    if (blur) errors.value.password = 'Password must contain at least one special character.'
+  } else {
+    errors.value.password = null
+  }
+}
+
+const validateResident = (blur) => {
+  if (!formData.value.isAustralian) {
+    if (blur) errors.value.resident = 'Australian residency must be confirmed.'
+  } else {
+    errors.value.resident = null
+  }
+}
+
+const validateGender = (blur) => {
+  if (!formData.value.gender) {
+    if (blur) errors.value.gender = 'Please select a gender.'
+  } else {
+    errors.value.gender = null
+  }
+}
+
+const validateReason = (blur) => {
+  if (formData.value.reason.trim().length < 10) {
+    if (blur) errors.value.reason = 'Reason must be at least 10 characters.'
+  } else {
+    errors.value.reason = null
+  }
+}
+
 const submitForm = () => {
-  submittedCards.value.push({
-    ...formData.value
-  })
+  validateName(true)
+  validatePassword(true)
+  validateResident(true)
+  validateGender(true)
+  validateReason(true)
+  if (
+    !errors.value.username &&
+    !errors.value.password &&
+    !errors.value.resident &&
+    !errors.value.gender &&
+    !errors.value.reason
+  ) {
+    submittedCards.value.push({
+      ...formData.value
+    })
+    clearForm()
+  }
 }
 const clearForm = () => {
   formData.value = {
--- fit5032-week2/tguo-library/src/main.js
+++ fit5032-week2/tguo-library/src/main.js
@@ -5,5 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
+
+app.mount('#app')
